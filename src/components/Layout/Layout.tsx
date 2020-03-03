76eb032a75d28f7476af7e3509851bd53e0373b3
Tom Quinn <tomquinn@hotmail.co.nz>
2020-03-04 02:44:32 +1300

    add linting + airbnb config

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Layout extends Component {
-
-    state = {
-        show: true
-    }
-
-    render() {
-        return (
-            <>
-                <div className="layout">
-                    <p>Placeholder!</p>
-                </div>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <div className="layout">
+          <p>Placeholder!</p>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Layout;
