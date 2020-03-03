eaeea1a35c58e51e57578de5c64a1a0d5a6b00d7
Tom Quinn <tomquinn@hotmail.co.nz>
2020-03-04 02:00:05 +1300

    initial tinkering w/ sass, layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Layout extends Component {
+
+    state = {
+        show: true
+    }
+
+    render() {
+        return (
+            <>
+                <div className="layout">
+                    <p>Placeholder!</p>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Layout;
