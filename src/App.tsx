91412e6eb192307f6493ffcf0d5fc0ea1c65ad6b
Tom Quinn <tomquinn@hotmail.co.nz>
2020-03-05 01:45:18 +1300

    configure basic auth0 login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import { useAuth0 } from './react-auth0-spa';
 import Layout from './components/Layout/Layout';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="app">
-      <Layout />
-    </div>
-  );
+
+    const { loading } = useAuth0();
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div className="app">
+            <header>
+                <NavBar />
+                <Layout />
+            </header>
+        </div>
+    )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Auth0Provider } from './react-auth0-spa';
+import config from './auth_config.json';
+import history from './utils/history';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.scss';
+
+interface AppState {
+    targetUrl: string;
+}
+
+// redirect the user back to the front page after login.
+const onRedirectCallback = (appState: AppState) => {
+    history.push(
+        appState && appState.targetUrl
+        ? appState.targetUrl : window.location.pathname
+    );
+};
+
+ReactDOM.render(
+    <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <App />
+    </Auth0Provider>,
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
